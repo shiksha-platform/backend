ca5a34f684f77ce0c1b1d31625ed925a75f8a632
Urvish Shah <shahurvish91@gmail.com>
2022-02-16 11:53:49 +0530

    Change entity name from group to topic

--- server/src/topic/topic.entity.ts
+++ server/src/topic/topic.entity.ts
@@ -1,7 +1,7 @@
 import { Entity, Column } from 'typeorm';
 import { BaseEntity } from '../model/base.entity';
 
-@Entity({ name: 'group' })
+@Entity({ name: 'topic' })
 export class Topic extends BaseEntity {
 
     @Column({ type: 'varchar', length: 300 })
