47d93aec780b011cccb115e2ffb6b44636900966
Urvish Shah <shahurvish91@gmail.com>
2022-02-25 12:03:09 +0530

    Use object resolver instead of mustache

--- server/src/student/student.service.ts
+++ server/src/student/student.service.ts
@@ -1,13 +1,13 @@
 import { Injectable, NotFoundException, HttpException, HttpStatus } from '@nestjs/common';
 import { catchError, map } from 'rxjs/operators';
 import {HttpService } from '@nestjs/axios'
+const resolvePath = require('object-resolve-path');
 
 import { StudentDto} from './dto/student.dto';
 import { ErrorResponse } from '../error-response';
 import { StudentResponseDto } from './dto/student-response.dto';
 import { StudentSearchDto } from './dto/student-search.dto';
 
-import Mustache = require("mustache");
 import { SaveStudentDto } from './dto/save-student.dto';
 import { IncomingHttpHeaders } from 'http';
 import { SuccessResponse } from '../success-response';
@@ -28,11 +28,17 @@ export class StudentService {
     return this.httpService.get(`${this.url}/${studentId}`, { headers: { Authorization: header.authorization } })
     .pipe(
         map(response => {
-          var output = Mustache.render(JSON.stringify(template), response.data);
+          const studentData = response.data;
+          const studentDto = new StudentDto(template);
+          Object.keys(template).forEach(key => {
+            studentDto[key] = resolvePath(studentData, template[key]);
+          });
+
+         /* var output = Mustache.render(JSON.stringify(template), response.data);*/
           return new SuccessResponse({
             statusCode: response.status,
             message: 'Student found Successfully',
-            data: new StudentDto(JSON.parse(output))
+            data: studentDto
           });
       }),
         catchError(e => {
@@ -49,22 +55,35 @@ export class StudentService {
  
 
   public async createStudent(header: IncomingHttpHeaders, studentDto: StudentDto) {
-      var responseTemplate = require('./../../response_templates/student/create_student_response.json');
-      var requestTemplate = require('./../../response_templates/student/create_student_request.json');
-      var input = Mustache.render(JSON.stringify(requestTemplate), studentDto);
-      return this.httpService.post(`${this.url}`,new SaveStudentDto(JSON.parse(input)),{ headers: { Authorization: header.authorization } })
+    var responseTemplate = require('./../../response_templates/student/create_student_response.json');
+    var requestTemplate = require('./../../response_templates/student/create_student_request.json');
+    // var input = Mustache.render(JSON.stringify(requestTemplate), studentDto);
+
+    const saveStudentDto = new SaveStudentDto(requestTemplate);
+    Object.keys(requestTemplate).forEach(key => {
+      saveStudentDto[key] = resolvePath(studentDto, requestTemplate[key]);
+    });
+
+    return this.httpService.post(`${this.url}`, saveStudentDto, {headers: {Authorization: header.authorization}})
       .pipe(
-          map(response => {
-          var output = Mustache.render(JSON.stringify(responseTemplate), response.data);
-          return new StudentResponseDto(JSON.parse(output))
+        map(response => {
+
+          const createStudentData = response.data;
+          const studentResponseDto = new StudentResponseDto(responseTemplate);
+          Object.keys(responseTemplate).forEach(key => {
+            studentResponseDto[key] = resolvePath(createStudentData, responseTemplate[key]);
+          });
+          return createStudentData;
+          /*var output = Mustache.render(JSON.stringify(responseTemplate), response.data);
+          return new StudentResponseDto(JSON.parse(output))*/
         }),
-          catchError(e => {
-            var error = new ErrorResponse({
-              errorCode : e.response.status,
-              errorMessage : e.response.data.params.errmsg
-            })
-            throw new HttpException(error, e.response.status);
+        catchError(e => {
+          var error = new ErrorResponse({
+            errorCode: e.response.status,
+            errorMessage: e.response.data.params.errmsg
           })
+          throw new HttpException(error, e.response.status);
+        })
       );
   }
 
@@ -72,12 +91,23 @@ export class StudentService {
   public async updateStudent(studentId:string, header: IncomingHttpHeaders, studentDto: StudentDto) {
     var responseTemplate = require('./../../response_templates/student/create_student_response.json');
     var requestTemplate = require('./../../response_templates/student/create_student_request.json');
-    var input = Mustache.render(JSON.stringify(requestTemplate), studentDto);
-    return this.httpService.put(`${this.url}/${studentId}`,new SaveStudentDto(JSON.parse(input)),{ headers: { Authorization: header.authorization } })
+    // var input = Mustache.render(JSON.stringify(requestTemplate), studentDto);
+    const updateStudentDto = new SaveStudentDto(requestTemplate);
+    Object.keys(requestTemplate).forEach(key => {
+      updateStudentDto[key] = resolvePath(studentDto, requestTemplate[key]);
+    });
+
+    return this.httpService.put(`${this.url}/${studentId}`,updateStudentDto,{ headers: { Authorization: header.authorization } })
     .pipe(
         map(response => {
-          var output = Mustache.render(JSON.stringify(responseTemplate), response.data);
-          return new StudentDto(JSON.parse(output))
+          const updateStudentData = response.data;
+          const studentDto = new StudentDto(responseTemplate);
+          Object.keys(responseTemplate).forEach(key => {
+            studentDto[key] = resolvePath(updateStudentData, responseTemplate[key]);
+          });
+          return updateStudentData;
+          /*var output = Mustache.render(JSON.stringify(responseTemplate), response.data);
+          return new StudentDto(JSON.parse(output))*/
       }),
         catchError(e => {
           var error = new ErrorResponse({
@@ -95,11 +125,17 @@ public async searchStudent(header: IncomingHttpHeaders, studentSearchDto: Studen
   .pipe(
       map(response => {
         return response.data.map(item =>{
-          var output = Mustache.render(JSON.stringify(template), item);
+          // var output = Mustache.render(JSON.stringify(template), item);
+          const searchStudentData = response.data;
+          const studentDto = new StudentDto(template);
+          Object.keys(template).forEach(key => {
+            studentDto[key] = resolvePath(searchStudentData, template[key]);
+          });
+
           return new SuccessResponse({
             statusCode: response.status,
             message: 'Student found Successfully',
-            data: new StudentDto(JSON.parse(output))
+            data: searchStudentData
           });
         });
        
@@ -132,11 +168,17 @@ public async findStudentByClass(searchClassId: String, header: IncomingHttpHeade
   .pipe(
       map(response => {
         return response.data.map(item =>{
-          var output = Mustache.render(JSON.stringify(template), item);
+          // var output = Mustache.render(JSON.stringify(template), item);
+          const findStudentData = response.data;
+          const studentDto = new StudentDto(template);
+          Object.keys(template).forEach(key => {
+            studentDto[key] = resolvePath(findStudentData, template[key]);
+          });
+
           return new SuccessResponse({
             statusCode: response.status,
             message: 'Student found Successfully',
-            data: new StudentDto(JSON.parse(output))
+            data: studentDto
           });
       });
     }),
